49b2a9569eaf195b49d9d61d06636efc3faac498
GustavoLuizFarignoli <gustavolf.educ@gmail.com>
2023-08-19 11:12:14 -0300

    Implementação da Pilha

---
+++ Pilha.java
@@ -0,0 +1,56 @@
+public class Pilha<T> implements Biblioteca {
+    private int top =-1;
+    private int size;
+    public T[] data;
+
+    public Pilha(int size) {
+        this.size = size;
+        this.data = (T[]) new Object[size];
+    }
+
+    @Override
+    public void push(Object data) {
+        if (isFull()){
+            System.out.println("Pilha Cheia");
+        } else {
+            this.top += 1;
+            this.data[this.top] = (T) data;
+        }
+    }
+
+    @Override
+    public T pop() {
+        if (isEmpty()){
+            System.out.println("A pilha já está vazia");
+            return null;
+        } else {
+            T removed = this.data[this.top];
+            this.data[this.top] = null;
+            this.top -= 1;
+            return removed;
+        }
+    }
+
+    @Override
+    public int size() {
+        return this.size;
+    }
+
+    @Override
+    public void clear() {
+        for (int i = 0; i < this.size; i++){
+            this.data[i] = null;
+            this.top = -1;
+        }
+    }
+
+    @Override
+    public Boolean isFull() {
+        return this.top == this.size-1;
+    }
+
+    @Override
+    public Boolean isEmpty() {
+        return this.top < 0;
+    }
+}
