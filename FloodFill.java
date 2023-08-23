6d69f80cab592a2545f1d71036b0a7c72420ef03
Gustavo Luiz Farignoli <gustavolf.educ@gmail.com>
2023-08-23 12:09:06 -0300

    começo da pjbl

---
+++ FloodFill.java
@@ -0,0 +1,12 @@
+public class FloodFill {
+    int voriginal;
+    int vnovo;
+    Pilha pilha;
+    Fila fila;
+
+    // reconhecer o que deve ser mudado
+
+    // aplicar mudança com pilha
+
+    // aplicar mudança com fila
+}
--- Main.java
+++ Main.java
@@ -1,11 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        ListaDuplamenteEncadeada l = new ListaDuplamenteEncadeada<>();
-        l.add(1);
-        l.add(2);
-        l.add(4);
-        l.printar();
-        System.out.println(l.remove());
-        l.printar();
+        int [][] matrix = {
+                {1,1,1,1,1,1,1,1,1,0},
+                {1,1,1,1,1,1,1,1,0,1},
+                {1,1,1,1,1,1,1,0,1,1},
+                {1,1,1,1,1,1,0,1,1,1},
+                {1,1,1,1,1,0,1,1,1,1},
+                {1,1,1,1,0,1,1,1,1,1},
+                {1,1,1,0,1,1,1,1,1,1},
+                {1,1,0,1,1,1,1,1,1,1},
+                {1,0,1,1,1,1,1,1,1,1},
+                {0,1,1,1,1,1,1,1,1,1}
+        };
     }
 }
+
