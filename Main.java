00d1ad7687ac581f8df3ad12cbb62d50bc5d6144
Molecote <gabrielmolec@outlook.com>
2023-08-27 19:33:04 -0300

    Adicionada um menu à classe Main.

--- Main.java
+++ Main.java
@@ -1,10 +1,25 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Bem vindo ao programa de Flood Fill feito por - Gustavo Farignoli, João Trigo e Gabriel Molec");
+        System.out.println("Gerando a matriz padrão");
         Matriz matriz = new Matriz();
+        System.out.println("Definindo a coordenada a ser inicial");
         Cordenada cordenada = new Cordenada(9,1);
+        System.out.println("Analisando coordenadas vizinhas");
         FloodFill floodFill = new FloodFill(cordenada,matriz,2);
         floodFill.foundcolorable(cordenada);
-        floodFill.colorpilha();
+        System.out.println("Escolha 1 para realizar a troca de valores através de Fila ou 2 para realizar com Pilha.");
+        int op = teclado.nextInt();
+        switch (op){
+            case 1:
+                floodFill.colorfila();
+                break;
+            case 2:
+                floodFill.colorpilha();
+                break;
+        }
     }
 }
 
